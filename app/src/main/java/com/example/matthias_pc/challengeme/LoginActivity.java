1f0832f184672dd42f2e77a6d45524724469e748
matthiasschuecker <matt.schuecker@gmx.at>
2019-01-06 23:12:29 +0100

    Implement basic register and login function
    Signed-off-by: matthiasschuecker <matt.schuecker@gmx.at>

--- app/src/main/java/com/example/matthias_pc/challengeme/LoginActivity.java
+++ app/src/main/java/com/example/matthias_pc/challengeme/LoginActivity.java
@@ -1,13 +1,59 @@
 package com.example.matthias_pc.challengeme;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    Button btnLogin;
+    private final static int LOGIN_PERMISSION = 1000;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder().build(), LOGIN_PERMISSION
+                );
+
+            }
+
+
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == LOGIN_PERMISSION){
+            startNewActivity(resultCode, data);
+        }
+    }
+
+    private void startNewActivity(int resultCode, Intent data) {
+        if(resultCode == RESULT_OK){
+            Intent intent = new Intent (LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            Toast.makeText(this,"Login failed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
